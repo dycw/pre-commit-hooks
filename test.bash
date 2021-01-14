58972e0f8e8b5ad8a45f2c27990dd4f1e03e801a
Derek Wan <d.wan@icloud.com>
2021-01-15 00:00:11 +0800

    more

--- test.bash
+++ test.bash
@@ -6,16 +6,19 @@ while read -r line; do
 	theArray+=("$line")
 done <<<"$files"
 
-for file in "${theArray[@]}"; do
-	echo then...
-	echo "file=$file"
-done
-
-
+# for file in "${theArray[@]}"; do
+# 	echo then...
+# 	echo "file=$file"
+# done
 
 
 root=$(git rev-parse --show-toplevel)
-hooks=$(find "$root"/hooks -type f)
+
+declare -a hooks
+while read -r hook; do
+	hooks+=("$hook")
+done <<< "$(find "$root"/hooks -type f)"
+
 for hook in "${hooks[@]}"; do
 	echo then...
 	echo "hook=$hook"
