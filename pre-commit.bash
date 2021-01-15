5dc4143e44946484bc4f00d5ebb735e95942c17a
Derek Wan <d.wan@icloud.com>
2021-01-15 11:29:07 +0800

    Try fix; use abs paths in pre-commit

--- pre-commit.bash
+++ pre-commit.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # shellcheck source=/dev/null
 
-run_hooks_on_file() {
+run_hooks_on_file_path() {
 	root=$(git rev-parse --show-toplevel)
 	code=0
 	while read -r hook; do
-		if ! "$hook" "$root/$file"; then
+		if ! "$hook" "$1"; then
 			code=1
 		fi
 	done <<<"$(find "$root/hooks" -type f)"
@@ -15,18 +15,19 @@ run_hooks_on_file() {
 run_hooks_on_all_files() {
 	root=$(git rev-parse --show-toplevel)
 	code=0
-	while read -r path; do
-		if [ -f "$path" ]; then
-			run_hooks_on_file "$path"
+	while read -r staged_file; do
+		path_staged="$root/$staged_file"
+		if [ -f "$path_staged" ]; then
+			run_hooks_on_file_path "$path_staged"
 			return $?
-		elif [ -d "$path" ]; then
+		elif [ -d "$path_staged" ]; then
 			code=0
-			while read -r file; do
-				if ! run_hooks_on_file "$file"; then
+			while read -r file_staged; do
+				if ! run_hooks_on_file_path "$root/$file_staged"; then
 					code=1
 				fi
-			done <<<"$(git ls-files "$path")"
-			return        $code
+			done <<<"$(git ls-files "$path_staged")"
+			return $code
 		else
 			return 0
 		fi
