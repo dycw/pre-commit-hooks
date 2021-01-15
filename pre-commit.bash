1a8a80746a11889df8acc097ad8666a652d7d23a
Derek Wan <d.wan@icloud.com>
2021-01-15 09:02:02 +0800

    Check no hooks

--- pre-commit.bash
+++ pre-commit.bash
@@ -2,16 +2,27 @@
 # shellcheck source=/dev/null
 
 root=$(git rev-parse --show-toplevel)
+hooks_dir="$root/hooks"
 
 declare -a hooks
 while read -r hook; do
 	hooks+=("$hook")
-done <<<"$(find "$root"/hooks -type f)"
+done <<<"$(find "$hooks_dir" -type f)"
+
+if [ ${#hooks[@]} -eq 0 ]; then
+	printf "No hooks found; please check %s" "$hooks_dir"
+	exit 1
+fi
 
 declare -a files
 while read -r file; do
 	files+=("$file")
 done <<<"$(git diff --name-only --cached)"
+
+if [ ${#files[@]} -eq 0 ]; then
+	printf "No files found"
+	exit 0
+fi
 
 code=0
 for hook in "${hooks[@]}"; do
