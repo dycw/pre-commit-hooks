b59bce109983ce633ec2363f68e83ed667ab9863
Derek Wan <d.wan@icloud.com>
2021-01-15 08:24:33 +0800

    Hi

--- pre-commit.bash
+++ pre-commit.bash
@@ -15,22 +15,24 @@ done <<<"$(git diff --name-only --cached)"
 
 code=0
 for hook in "${hooks[@]}"; do
-	hook_name=$(basename "$hook")
+	# hook_name=$(basename "$hook")
 	for file in "${files[@]}"; do
 		full_file="$root/$file"
-		# echo "file=$file"
-		# echo "hook=$hook"
-		# echo "full_file=$full_file"
 		(
 			set -e
 			source "$hook" "$full_file"
 		)
-		inner_code=$?
-		if [ $inner_code -eq 0 ]; then
-			echo "OK with $hook_name on $file"
+		# inner_code=$?
+		# if [ $inner_code -eq 0 ]; then
+		# 	echo "OK with $hook_name on $file"
 
-		else
-			echo "Failed with $hook_name on $file"
+		# else
+		# 	echo "Failed with $hook_name on $file"
+		# 	code=1
+		# fi
+
+		inner_code=$?
+		if [ $inner_code -ne 0 ]; then
 			code=1
 		fi
 	done
@@ -38,7 +40,8 @@ done
 
 if [ $code -eq 0 ]; then
 	exit 0
-
 else
 	exit 1
 fi
+
+exit $code
