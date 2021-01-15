4434cfcf60c6f2ac291a7600db865c57ec957468
Derek Wan <d.wan@icloud.com>
2021-01-15 09:19:26 +0800

    arst

--- pre-commit.bash
+++ pre-commit.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 # shellcheck source=/dev/null
 
+declare -a files
+while read -r file; do
+	if [ -n "$file" ]; then
+		files+=("$file")
+	fi
+done <<<"$(git diff --name-only --cached)"
+
 root=$(git rev-parse --show-toplevel)
 hooks_dir="$root/hooks"
 
@@ -9,41 +16,34 @@ while read -r hook; do
 	hooks+=("$hook")
 done <<<"$(find "$hooks_dir" -type f)"
 
-if [ ${#hooks[@]} -eq 0 ]; then
-	printf "No hooks found; please check %s\n" "$hooks_dir"
-	exit 1
-fi
-
-declare -a files
-while read -r file; do
-	if [ -n "$file" ]; then
-		files+=("$file")
-	fi
-done <<<"$(git diff --name-only --cached)"
-
-if [ ${#files[@]} -eq 0 ]; then
-	printf "No files found\n"
-	exit 0
-else
-	printf "Files found\n"
-	for file in "${files[@]}"; do
-		printf "We have files to: %s\n" "$file"
-	done
-
-fi
+# if [ ${#hooks[@]} -eq 0 ]; then
+# 	printf "No hooks found; please check %s\n" "$hooks_dir"
+# 	exit 1
+# fi
 
 code=0
-for hook in "${hooks[@]}"; do
-	for file in "${files[@]}"; do
-		full_file="$root/$file"
+for file in "${files[@]}"; do
+	full_file="$root/$file"
+	for hook in "${hooks[@]}"; do
 		if ! . "$hook" "$full_file"; then
 			code=1
 		fi
 	done
+	git add "$full_file"
 done
 
-if [ $code -eq 0 ]; then
-	foo=$(git diff --cached --exit-code)
-	echo "foo=$foo"
+if [ $code -ne 0 ]; then
+	exit $code
 	# git status --untracked-files=no --porcelain
+fi
+
+declare -a files2
+while read -r file2; do
+	if [ -n "$file2" ]; then
+		files2+=("$file2")
+	fi
+done <<<"$(git diff --name-only --cached)"
+if [ ${#files2[@]} -eq 0 ]; then
+	printf "No files found at this stage\n"
+	exit 1
 fi
