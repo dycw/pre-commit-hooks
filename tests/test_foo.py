b7a21b063a8706bcd220d121a40ed6e7ede57e48
Derek Wan <d.wan@icloud.com>
2021-01-15 19:25:57 +0800

    Remove commented

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -5,9 +5,8 @@ from pytest import mark
 
 
 @mark.xfail(reason="arst", until=datetime.date(2021, 4, 4))
-# @pytest.mark.xfail(reason="arst")
 def test_main() -> None:
-    x = 3
+    x = 4
     assert x > 0
 
 
