95b62923a2ce754faa922062472c955a25b565b2
Derek Wan <d.wan@icloud.com>
2021-01-15 10:50:11 +0800

    Correctly handle to-check logic

--- hooks/shfmt-and-shellcheck.bash
+++ hooks/shfmt-and-shellcheck.bash
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
-handle_file() {
-	echo "$1 here"
+to_check() {
 	if [ "$1" == "*.bash" ]; then
-		echo "$1"
 		return 0
 	fi
 	line=$(head -n 1 "$1")
-	if [[ "$line" != *bash* ]]; then
-		return 1 # this is untested
+	if [[ "$line" == *bash* ]]; then
+		return 0 # this is untested
+	else
+		return 1
+	fi
+}
+
+handle_file() {
+	if ! to_check "$1"; then
+		return 0
 	fi
 
-	# have a bash file now
 	root=$(git rev-parse --show-toplevel)
 	desc=$(realpath --relative-to="$root" "$1")
 	printf "Running shfmt > %s\n" "$desc"
@@ -20,14 +25,20 @@ handle_file() {
 	fi
 	git add "$1"
 	printf "Running shellcheck > %s\n" "$desc"
-	shellcheck "$1"
-	return $?
+	if shellcheck "$1"; then
+		return 0
+	else
+		return 1
+	fi
 }
 
 handle_path() {
 	if [ -f "$1" ]; then
-		handle_file "$1"
-		return $?
+		if handle_file "$1"; then
+			return 0
+		else
+			return 1
+		fi
 	elif [ -d "$1" ]; then
 		failed=0
 		while read -r file; do
@@ -42,9 +53,7 @@ handle_path() {
 		fi
 	else
 		return 0
-
 	fi
-
 }
 
 if handle_path "$1"; then
