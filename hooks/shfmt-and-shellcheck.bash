ef33777c9aff978b38d64877a709613cd36f70bf
Derek Wan <d.wan@icloud.com>
2021-01-15 08:06:57 +0800

    WOP

--- hooks/shfmt-and-shellcheck.bash
+++ hooks/shfmt-and-shellcheck.bash
@@ -1,32 +1,25 @@
 #!/usr/bin/env bash
 # here="$(dirname "$(readlink -f "$0")")"
 
+if [ "$#" -eq 1 ]; then
+	printf "Running shmft on %s...\n" "$1"
+	shfmt -w "$1"
+	shfmt_code=$?
+	printf "shfmt returned %s\n" "$shfmt_code"
+	if [ $shfmt_code -eq 0 ]; then
+		printf "Running shellcheck on %s...\n" "$1"
+		shellcheck "$1"
+		shellcheck_code="$?"
+		printf "shellcheck returned \n%s" "$shellcheck_code"
+		return shellcheck_code
+	else
 
-
-if [ "$#" -eq 1 ]
-then
-    printf "Running shmft on %s..." "$1"
-    shfmt -w "$1"
-    shfmt_code=$?
-		printf "shfmt returned %s" "$shfmt_code"
-    if [ $shfmt_code -eq 0 ]; then
-      printf "Running shellcheck on %s..." "$1"
-      shellcheck "$1"
-      shellcheck_code="$?"
-      printf "shellcheck returned $shellcheck_code"
-      return shellcheck_code
-		else
-
-
-
-			printf "shfmt returnd $shfmt_code"
-      return $shfmt
-		fi
+		printf "shfmt returnd %s\n" "$shfmt_code"
+		return $shfmt
+	fi
 
 else
-  here="$(readlink -f "$0")"
-  printf "%s expects exactly 1 parameter; got $#"
-    exit 1
+	here="$(readlink -f "$0")"
+	printf "%s expects exactly 1 parameter; got %s\n" "$here" "$#"
+	exit 1
 fi
-
-
