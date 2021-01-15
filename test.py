88e5fbccc185b25ace3941d0a08e8917b3d0eaf4
Derek Wan <d.wan@icloud.com>
2021-01-15 18:22:51 +0800

    WIP flake8-ignores file

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-def fooo():
+def fooo(
+    aarst: int,  # noqa:U100
+    strstnwift: int,  # noqa:U100
+    sriulxcvyuf: int,  # noqa:U100
+    fpqwfitars: int,  # noqa:U100
+    st: int,  # noqa:U100
+    ntaiesnta: int,  # noqa:U100
+    pynrstur: int,  # noqa:U100
+    rnsitearnst: int,  # noqa:U100
+    wpuywqfntarst: int,  # noqa:U100
+    nrsytrsntr_trsta: int,  # noqa:U100
+    arnstiars: int,  # noqa:U100
+) -> int:
     a = 1
     return a
