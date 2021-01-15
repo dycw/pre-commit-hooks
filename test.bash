c697ac6942245fda63f0676bc906bb2fc1b93777
Derek Wan <d.wan@icloud.com>
2021-01-15 09:12:12 +0800

    arst

--- test.bash
+++ test.bash
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
 
-declare -a files
-while read -r line; do
-	if ! [ -z "$line"]
-	then
-	echo "Adding '$line'..."
-	files+=("$line")
-	fi
-done <<<$(git diff --name-only --cached)
-
-for file in "${files[@]}"; do
-	echo then...
-	echo "file=$file"
-done
-
-if [ ${#files[@]} -eq 0 ]; then
-    echo "No files"
-else
-    echo "Yes files"
-fi
+a=1
+echo $a
