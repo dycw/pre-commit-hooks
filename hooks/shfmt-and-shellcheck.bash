fe577548bf63e09507652819ef646bb8a7b861d5
Derek Wan <d.wan@icloud.com>
2021-01-15 10:46:51 +0800

    Fix linter

--- hooks/shfmt-and-shellcheck.bash
+++ hooks/shfmt-and-shellcheck.bash
@@ -1,53 +1,54 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-	printf "Expected exactly 1 parameter; got %s\n" "$#"
-	exit 1
-fi
-
-maybe_process_file() {
+handle_file() {
 	echo "$1 here"
 	if [ "$1" == "*.bash" ]; then
 		echo "$1"
-	exit 0
+		return 0
 	fi
 	line=$(head -n 1 "$1")
 	if [[ "$line" != *bash* ]]; then
-			exit 1
+		return 1 # this is untested
 	fi
 
 	# have a bash file now
 	root=$(git rev-parse --show-toplevel)
 	desc=$(realpath --relative-to="$root" "$1")
-		echo trying shfmt "$1"
-		if ! shfmt -w "$1"; then
-			exit 1
-		fi
-		git add "$1"
-		if shellcheck "$1"; then
-		exit 0
-		else
-		exit 1
-		fi
+	printf "Running shfmt > %s\n" "$desc"
+	if ! shfmt -w "$1"; then
+		return 1
+	fi
+	git add "$1"
+	printf "Running shellcheck > %s\n" "$desc"
+	shellcheck "$1"
+	return $?
 }
 
-
-if [ -f "$1 "];   then
-	maybe_process_file "$1"
+handle_path() {
+	if [ -f "$1" ]; then
+		handle_file "$1"
+		return $?
 	elif [ -d "$1" ]; then
 		failed=0
 		while read -r file; do
-			if ! maybe_process_file "$file"; then
+			if handle_file "$file"; then
 				failed=1
 			fi
 		done <<<"$(git ls-files "$1")"
-		if [  $failed -eq 0 ]
-		then
-		exit 0
+		if [ $failed -eq 0 ]; then
+			return 0
 		else
-		exit 1
+			return 1
 		fi
 	else
-		exit 0
+		return 0
+
 	fi
+
+}
+
+if handle_path "$1"; then
+	exit 0
+else
+	exit 1
 fi
