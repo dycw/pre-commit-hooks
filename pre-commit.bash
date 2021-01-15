9f93d1858e7279134a88749c7e877eeaeca8ca81
Derek Wan <d.wan@icloud.com>
2021-01-15 10:10:47 +0800

    Refactor so commands are done inside nested loops

--- pre-commit.bash
+++ pre-commit.bash
@@ -1,47 +1,31 @@
 #!/usr/bin/env bash
 # shellcheck source=/dev/null
 
-declare -a pre_files
-while read -r file; do
-	if [ -n "$file" ]; then
-		pre_files+=("$file")
-	fi
-done <<<"$(git diff --name-only --cached)"
-
 root=$(git rev-parse --show-toplevel)
-hooks_dir="$root/hooks"
-
-declare -a hooks
-while read -r hook; do
-	hooks+=("$hook")
-done <<<"$(find "$hooks_dir" -type f)"
-
-if [ ${#hooks[@]} -eq 0 ]; then
-	printf "No hooks found; please check %s\n" "$hooks_dir"
-	exit 1
-fi
-
 code=0
-for file in "${pre_files[@]}"; do
-	full_file="$root/$file"
-	for hook in "${hooks[@]}"; do
-		if ! "$hook" "$full_file"; then
-			code=1
-		fi
-	done
-done
-
-if [ $code -ne 0 ]; then
-	exit $code
-fi
-
-declare -a post_files
 while read -r file; do
 	if [ -n "$file" ]; then
-		post_files+=("$file")
+		full_file="$root/$file"
+		while read -r hook; do
+			if ! "$hook" "$full_file"; then
+				code=1
+			fi
+		done <<<"$(find "$root/hooks" -type f)"
 	fi
 done <<<"$(git diff --name-only --cached)"
 
-if [ ${#post_files[@]} -ne 0 ]; then
-	exit 0
+if [ $code -eq 0 ]; then
+	declare -a post_files
+	while read -r file; do
+		if [ -n "$file" ]; then
+			post_files+=("$file")
+		fi
+	done <<<"$(git diff --name-only --cached)"
+	if [ ${#post_files[@]} -eq 0 ]; then
+		exit 1
+	else
+		exit 0
+	fi
+else
+	exit $code
 fi
