ac2381c60a9f82ffbb2e5ff1cde3b016ec1898f6
Derek Wan <d.wan@icloud.com>
2021-01-15 10:21:18 +0800

    Add _is_file

--- hooks/shfmt-and-shellcheck.bash
+++ hooks/shfmt-and-shellcheck.bash
@@ -1,32 +1,50 @@
 #!/usr/bin/env bash
 
+_is_file() {
+	echo "Checking if $1 is a bash file"
+	if [ -f "$1" ]; then
+		if [[ "$1" == *.bash ]]; then
+			line=$(head -n 1 "$1")
+			if [[ "$line" == *bash* ]]; then
+				exit 0
+			else
+				exit 1
+			fi
+		else
+			exit 0
+		fi
+	else
+		exit 1
+	fi
+}
+
 if [ "$#" -eq 1 ]; then
-	path="$1"
-	if [ -f "$path" ]; then
+	if [ _is_file "$1" ]; then
 		root=$(git rev-parse --show-toplevel)
 		desc=$(realpath --relative-to="$root" "$1")
 		printf "shfmt -> %s\n" "$desc"
-		shfmt -w "$path"
+		shfmt -w "$1"
 		shfmt_code=$?
 		if [ $shfmt_code -eq 0 ]; then
-			git add "$path"
+			git add "$1"
 			printf "shellcheck -> %s\n" "$desc"
-			shellcheck "$path"
+			shellcheck "$1"
 			exit $?
 		else
 			exit $shfmt_code
 		fi
-	elif [ -d "$path" ]; then
+	elif [ -d "$1" ]; then
 		code=0
 		while read -r file; do
 			if ! shfmt-and-shellcheck "$file"; then
 				code=1
 			fi
-		done <<<"$(git ls-files "$path")"
+		done <<<"$(git ls-files "$1")"
 		exit $code
+	else
+		exit 0
 	fi
 else
-	here="$(readlink -f "$0")"
-	printf "%s expects exactly 1 parameter; got %s\n" "$here" "$#"
+	printf "Expected exactly 1 parameter; got %s\n" "$#"
 	exit 1
 fi
