fec7144a7eb13f15f25cdba03c90a5927d2b810b
Derek Wan <d.wan@icloud.com>
2021-01-15 19:23:18 +0800

    Use .flake8

--- tests/just_a_file.py
+++ tests/just_a_file.py
@@ -5,13 +5,11 @@ def fooo(
     fpqwfitars: int,  # noqa:U100
     st: int,  # noqa:U100
     ntaiesnta: int,  # noqa:U100
-    pynrstur: int,  # noqa:U100
+    pynrstur: int,
     rnsitearnst: int,  # noqa:U100
     wpuywqfntarst: int,  # noqa:U100
-    nrsytrsntr_trsta: int,  # noqa:U100
-    arnstiars: int,  # noqa:U100
-    _,
+    nrsytrsntr_trsta: int,
 ) -> int:
     a = 1
 
-    return a
+    return a + nrsytrsntr_trsta + pynrstur
