bfdcfe0ff80b9bd86bd51e9ab8f5ef1be9a1d042
Derek Wan <d.wan@icloud.com>
2021-01-20 00:19:14 +0800

    Major update: move back to pre-commit

--- setup.py
+++ setup.py
@@ -2,28 +2,57 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-def foo(aaaaaaaaaa, bbbbbbbbbb, cccccccccccccc, ddddddddddd, eeeeeeeee, fffffff) -> None:
-    pass
-
-
 setup(
-    name="dycw-pre-commit-hooks",
-    description="DYCW's pre-commit hooks",
-    url="https://github.com/dycw/pre-commit-hooks",
-    author="Derek Wan",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
+    name="dummy-package",
+    entry_points={
+        "console_scripts": [
+            "do-python = hooks.do_python:main",
+            "no-commit = hooks.no_commit:main",
+            "text = hooks.text:main",
+            "universal = hooks.universal:main",
+        ],
+    },
+    include_package_data=True,
+    install_requires=[
+        # generic
+        "pre-commit-hooks",
+        # formatters
+        "add-trailing-comma",
+        "autoflake",
+        "black",
+        "pyupgrade",
+        "reorder-python-imports",
+        "yesqa",
+        # linters
+        "flake8",
+        "mypy",
+        # flake8
+        "dlint",
+        "flake8-absolute-import",
+        "flake8-annotations",
+        "flake8-bandit",
+        "flake8-bugbear",
+        "flake8-builtins",
+        "flake8-commas",
+        "flake8-comprehensions",
+        "flake8-debugger",
+        "flake8-eradicate",
+        "flake8-executable",
+        "flake8-fine-pytest",
+        "flake8-fixme",
+        "flake8-future-import",
+        "flake8-implicit-str-concat",
+        "flake8-mutable",
+        "flake8-print",
+        "flake8-pytest-style",
+        "flake8-simplify",
+        "flake8-string-format",
+        "flake8-todo",
+        "flake8-typing-imports",
+        "flake8-unused-arguments",
+        "pep8-naming",
     ],
     packages=find_packages(),
-    install_requires=["black"],
-    # not sure:     entry_points={
-    # not sure:         'console_scripts': [
-    # not sure:             'terraform_docs_replace = pre_commit_hooks.terraform_docs_replace:main',
-    # not sure:         ],
-    # not sure:     },
+    package_data={"python": [".flake8", "mypy.ini"]},
+    url="https://github.com/dycw/pre-commit-hooks",
 )
