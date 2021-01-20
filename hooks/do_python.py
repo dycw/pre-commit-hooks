98fcac4b9275f797803a228ce37488c7ee67844f
Derek Wan <d.wan@icloud.com>
2021-01-20 10:15:50 +0800

    Factor out py_hooks so both do_python and notebook can call

--- hooks/do_python.py
+++ hooks/do_python.py
@@ -1,43 +1,37 @@
 from argparse import ArgumentParser
 from contextlib import contextmanager
 from importlib import resources
-from pathlib import Path
 from subprocess import CalledProcessError  # noqa:S404
 from subprocess import check_call  # noqa:S404
 from sys import exit
 from typing import Iterator
+from typing import List
 from typing import Optional
 from typing import Sequence
 
 
-def process_file(file: str, add_trailing_comma: str, flake8: Path, mypy: Path) -> bool:
+def wrapped_check_call(args: List[str], file: str) -> None:
+    check_call(args + [file])  # noqa: S603
+
+
+def process_file(
+    file: str,
+    add_trailing_comma: List[str],
+    autoflake: List[str],
+    pyupgrade: List[str],
+    reorder_python_imports: List[str],
+    flake8: List[str],
+    mypy: List[str],
+) -> bool:
     try:
-        check_call([add_trailing_comma, file])  # noqa: S603
-        check_call(  # noqa:S603,S607
-            [
-                "autoflake",
-                "--in-place",
-                "--remove-all-unused-imports",
-                "--remove-duplicate-keys",
-                "--remove-unused-variables",
-                file,
-            ],
-        )
-        check_call(  # noqa:S603,S607
-            ["pyupgrade", "--exit-zero-even-if-changed", "--py38-plus", file],
-        )
-        check_call(  # noqa:S603,S607
-            [
-                "reorder-python-imports",
-                "--exit-zero-even-if-changed",
-                "--py38-plus",
-                file,
-            ],
-        )
-        check_call(["yesqa", file])  # noqa:S603,S607
-        check_call(["black", file])  # noqa:S603,S607
-        check_call(["flake8", f"--config={flake8}", file])  # noqa:S603,S607
-        check_call(["mypy", f"--config={mypy}", file])  # noqa:S603,S607
+        wrapped_check_call(add_trailing_comma, file)
+        wrapped_check_call(autoflake, file)
+        wrapped_check_call(pyupgrade, file)
+        wrapped_check_call(reorder_python_imports, file)
+        wrapped_check_call(["yesqa"], file)
+        wrapped_check_call(["black"], file)
+        wrapped_check_call(flake8, file)
+        wrapped_check_call(mypy, file)
     except CalledProcessError:
         return False
     else:
@@ -49,21 +43,33 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser.add_argument("filenames", nargs="*")
     args = parser.parse_args(argv)
 
-    def read_call(name: str) -> str:
-        return resources.read_text("hooks.py_hooks", name).replace("\n", " ")
+    def read_call(name: str) -> List[str]:
+        return resources.read_text("hooks.py_hooks", name).splitlines()
 
     add_trailing_comma = read_call("add_trailing_comma")
+    autoflake = read_call("autoflake")
+    pyupgrade = read_call("pyupgrade")
+    reorder_python_imports = read_call("reorder_python_imports")
 
     @contextmanager
-    def yield_config(name: str) -> Iterator[Path]:
-        with resources.path("hooks.py_hooks", name) as path:
-            yield path
+    def yield_call(cmd: str, file: str) -> Iterator[List[str]]:
+        with resources.path("hooks.py_hooks", file) as path:
+            yield [cmd, f"--config={path}"]
 
-    with yield_config(".flake8") as flake8, yield_config(
+    with yield_call("flake", ".flake8") as flake8, yield_call(
+        "mypy",
         "mypy.ini",
     ) as mypy:
         result = all(
-            process_file(file, add_trailing_comma, flake8, mypy)
+            process_file(
+                file,
+                add_trailing_comma,
+                autoflake,
+                pyupgrade,
+                reorder_python_imports,
+                flake8,
+                mypy,
+            )
             for file in args.filenames
         )
     return 0 if result else 1
--- hooks/notebook.py
+++ hooks/notebook.py
@@ -1,34 +1,40 @@
 from argparse import ArgumentParser
+from contextlib import contextmanager
+from importlib import resources
 from subprocess import CalledProcessError  # noqa:S404
 from subprocess import check_call  # noqa:S404
 from sys import exit
+from typing import Iterator
+from typing import List
 from typing import Optional
 from typing import Sequence
 
 
-def process_file(file: str) -> bool:
+def wrapped_check_call(args: List[str], file: str, *, mutate: bool) -> None:
+    check_call(  # noqa: S603
+        ["nbqa"] + args + (["--nbqa-mutate"] if mutate else []) + [file],
+    )
+
+
+def process_file(
+    file: str,
+    add_trailing_comma: List[str],
+    autoflake: List[str],
+    pyupgrade: List[str],
+    reorder_python_imports: List[str],
+    flake8: List[str],
+    mypy: List[str],
+) -> bool:
     try:
-        check_call(  # noqa:S603,S607
-            [
-                "nbqa",
-                "autoflake",
-                "--in-place",
-                "--remove-all-unused-imports",
-                "--remove-duplicate-keys",
-                "--remove-unused-variables",
-                file,
-                "--nbqa-mutate",
-            ],
-        )
-        check_call(  # noqa:S603,S607
-            [
-                "nbqa",
-                "black",
-                file,
-                "--nbqa-mutate",
-            ],
-        )
-        check_call(["nbstripout", file])  # noqa:S603,S607
+        wrapped_check_call(add_trailing_comma, file, mutate=True)
+        wrapped_check_call(autoflake, file, mutate=True)
+        wrapped_check_call(pyupgrade, file, mutate=True)
+        wrapped_check_call(reorder_python_imports, file, mutate=True)
+        wrapped_check_call(["yesqa"], file, mutate=True)
+        wrapped_check_call(["black"], file, mutate=True)
+        wrapped_check_call(["nbstripout"], file, mutate=False)
+        wrapped_check_call(flake8, file, mutate=False)
+        wrapped_check_call(mypy, file, mutate=False)
     except CalledProcessError:
         return False
     else:
@@ -39,7 +45,36 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = ArgumentParser()
     parser.add_argument("filenames", nargs="*")
     args = parser.parse_args(argv)
-    result = all(process_file(file) for file in args.filenames)
+
+    def read_call(name: str) -> List[str]:
+        return resources.read_text("hooks.py_hooks", name).splitlines()
+
+    add_trailing_comma = read_call("add_trailing_comma")
+    autoflake = read_call("autoflake")
+    pyupgrade = read_call("pyupgrade")
+    reorder_python_imports = read_call("reorder_python_imports")
+
+    @contextmanager
+    def yield_call(cmd: str, file: str) -> Iterator[List[str]]:
+        with resources.path("hooks.py_hooks", file) as path:
+            yield [cmd, f"--config={path}"]
+
+    with yield_call("flake", ".flake8") as flake8, yield_call(
+        "mypy",
+        "mypy.ini",
+    ) as mypy:
+        result = all(
+            process_file(
+                file,
+                add_trailing_comma,
+                autoflake,
+                pyupgrade,
+                reorder_python_imports,
+                flake8,
+                mypy,
+            )
+            for file in args.filenames
+        )
     return 0 if result else 1
 
 
