bcac601cb108a95029b0dfd24f38390686492e24
Derek Wan <d.wan@icloud.com>
2021-02-21 20:16:59 +0800

    Remove prospector

--- hooks/check_settings.py
+++ hooks/check_settings.py
@@ -36,6 +36,16 @@ def check_envrc() -> None:
             raise ValueError(f"Incorrect environment: {current}")
 
 
+def check_hook_fields(
+    repo_hooks: Dict[str, Any],
+    expected_mapping: Dict[str, List[str]],
+    field: str,
+) -> None:
+    for hook_name, expected in expected_mapping.items():
+        current = repo_hooks[hook_name][field]
+        check_lists_equal(current, expected, f"{hook_name}.{field}")
+
+
 def check_lists_equal(current: List[str], expected: List[str], desc: str) -> None:
     if current != sorted(current):
         raise ValueError(f"{desc} actual is unsorted: {current}")
@@ -47,97 +57,19 @@ def check_lists_equal(current: List[str], expected: List[str], desc: str) -> Non
         raise ValueError(f"{desc} is missing: {missing}")
 
 
-def check_pyrightconfig_json() -> None:
-    get_environment_name()
-    with open(get_repo_root().joinpath("pyrightconfig.json")) as file:
-        pyrightconfig = json.load(file)
-    venv_path = pyrightconfig["venvPath"]
-    venv = pyrightconfig["venv"]
-    if venv != get_environment_name():
-        raise ValueError(f"Incorrect environment: {venv}")
-    if getenv("PRE_COMMIT_CI", "0") != "1" and not (
-        path := Path(venv_path, venv).exists()
-    ):
-        raise FileNotFoundError(path)
-
-
-def check_pyproject_toml_black(file: TextIO) -> None:
-    pyproject = toml.load(file)
-    black = pyproject["tool"]["black"]
-    if (line_length := black["line-length"]) != 88:
-        raise ValueError(f"Incorrect line length: {line_length}")
-    if (target_version := black["target-version"]) != ["py38"]:
-        raise ValueError(f"Incorrect target version: {target_version}")
-
-
-def check_repo(
-    repo_url: str,
-    *,
-    enabled_hooks: Optional[List[str]] = None,
-    hook_args: Optional[Dict[str, List[str]]] = None,
-    hook_additional_dependencies: Optional[Dict[str, List[str]]] = None,
-    config_filename: Optional[str] = None,
-    config_checker: Optional[Callable[[TextIO], None]] = None,
-    config_remote: Optional[str] = None,
-) -> None:
-    repos = get_pre_commit_repos()
-    try:
-        repo = repos[repo_url]
-    except KeyError:
-        return
-
-    repo_hooks = get_repo_hooks(repo)
-    if enabled_hooks is not None:
-        check_lists_equal(
-            current=list(repo_hooks),
-            expected=enabled_hooks,
-            desc="hook set",
-        )
-    if hook_args is not None:
-        check_hook_fields(repo_hooks, hook_args, "args")
-    if hook_additional_dependencies is not None:
-        check_hook_fields(
-            repo_hooks,
-            hook_additional_dependencies,
-            "additional_dependencies",
-        )
-
-    config_filename_absent = ValueError('"config_filename" is absent')
-    if config_checker is not None and config_remote is None:
-        if config_filename is None:
-            raise config_filename_absent
-        with open(get_repo_root().joinpath(config_filename)) as file:
-            config_checker(file)
-    elif config_remote is not None and config_checker is None:
-        if config_filename is None:
-            raise config_filename_absent
-        check_local_vs_remote(config_filename, config_remote)
-    elif config_checker is not None and config_remote is not None:
-        raise ValueError('"config_checker" and "config_remote" are mutually exclusive')
-
-
-def check_hook_fields(
-    repo_hooks: Dict[str, Any],
-    expected_mapping: Dict[str, List[str]],
-    field: str,
-) -> None:
-    for hook_name, expected in expected_mapping.items():
-        current = repo_hooks[hook_name][field]
-        check_lists_equal(current, expected, f"{hook_name}.{field}")
-
-
-def check_local_vs_remote(filename: str, url: str) -> None:
+def check_local_vs_remote(filename: str) -> None:
     local_path = get_repo_root().joinpath(filename)
+    remote_url = get_github_file(filename)
     try:
         with open(local_path) as file:
             local = file.read()
     except FileNotFoundError:
         info(f"{local_path} not found; creating...")
-        write_local(local_path, url)
+        write_local(local_path, remote_url)
     else:
-        if local != read_remote(url):
+        if local != read_remote(remote_url):
             info(f"{local_path} is out-of-sync; updating...")
-            write_local(local_path, url)
+            write_local(local_path, remote_url)
 
 
 def check_pre_commit_config() -> None:
@@ -173,7 +105,7 @@ def check_pre_commit_config() -> None:
         repo_url="https://github.com/PyCQA/flake8",
         hook_additional_dependencies={"flake8": get_flake8_dependencies()},
         config_filename=".flake8",
-        config_remote="https://raw.githubusercontent.com/dycw/pre-commit-hooks/master/.flake8",
+        config_remote=True,
     )
     check_repo(
         repo_url="https://github.com/jumanjihouse/pre-commit-hooks",
@@ -182,7 +114,7 @@ def check_pre_commit_config() -> None:
     check_repo(
         repo_url="https://github.com/pre-commit/mirrors-mypy",
         config_filename="mypy.ini",
-        config_remote="https://raw.githubusercontent.com/dycw/pre-commit-hooks/master/mypy.ini",
+        config_remote=True,
     )
     check_repo(
         repo_url="https://github.com/pre-commit/pre-commit-hooks",
@@ -205,12 +137,35 @@ def check_pre_commit_config() -> None:
     check_repo(
         repo_url="https://github.com/PyCQA/pylint",
         config_filename=".pylintrc",
-        config_remote="https://raw.githubusercontent.com/dycw/pre-commit-hooks/master/.pylintrc",
+        config_remote=True,
     )
     check_repo(
         repo_url="https://github.com/asottile/yesqa",
         hook_additional_dependencies={"yesqa": get_flake8_dependencies()},
     )
+
+
+def check_pyproject_toml_black(file: TextIO) -> None:
+    pyproject = toml.load(file)
+    black = pyproject["tool"]["black"]
+    if (line_length := black["line-length"]) != 88:
+        raise ValueError(f"Incorrect line length: {line_length}")
+    if (target_version := black["target-version"]) != ["py38"]:
+        raise ValueError(f"Incorrect target version: {target_version}")
+
+
+def check_pyrightconfig_json() -> None:
+    get_environment_name()
+    with open(get_repo_root().joinpath("pyrightconfig.json")) as file:
+        pyrightconfig = json.load(file)
+    venv_path = pyrightconfig["venvPath"]
+    venv = pyrightconfig["venv"]
+    if venv != get_environment_name():
+        raise ValueError(f"Incorrect environment: {venv}")
+    if getenv("PRE_COMMIT_CI", "0") != "1" and not (
+        path := Path(venv_path, venv).exists()
+    ):
+        raise FileNotFoundError(path)
 
 
 def check_pytest_ini() -> None:
@@ -230,6 +185,52 @@ def check_pytest_ini() -> None:
             raise ValueError(f"looponfailroots missing: {path}")
     if (minversion := pytest["minversion"]) != "6.0":
         raise ValueError(f"Incorrect min version: {minversion}")
+
+
+def check_repo(
+    repo_url: str,
+    *,
+    enabled_hooks: Optional[List[str]] = None,
+    hook_args: Optional[Dict[str, List[str]]] = None,
+    hook_additional_dependencies: Optional[Dict[str, List[str]]] = None,
+    config_filename: Optional[str] = None,
+    config_checker: Optional[Callable[[TextIO], None]] = None,
+    config_remote: bool = False,
+) -> None:
+    repos = get_pre_commit_repos()
+    try:
+        repo = repos[repo_url]
+    except KeyError:
+        return
+
+    repo_hooks = get_repo_hooks(repo)
+    if enabled_hooks is not None:
+        check_lists_equal(
+            current=list(repo_hooks),
+            expected=enabled_hooks,
+            desc="hook set",
+        )
+    if hook_args is not None:
+        check_hook_fields(repo_hooks, hook_args, "args")
+    if hook_additional_dependencies is not None:
+        check_hook_fields(
+            repo_hooks,
+            hook_additional_dependencies,
+            "additional_dependencies",
+        )
+
+    config_filename_absent = ValueError('"config_filename" is absent')
+    if config_checker is not None and config_remote:
+        if config_filename is None:
+            raise config_filename_absent
+        with open(get_repo_root().joinpath(config_filename)) as file:
+            config_checker(file)
+    elif config_remote and config_checker is None:
+        if config_filename is None:
+            raise config_filename_absent
+        check_local_vs_remote(config_filename)
+    elif config_checker is not None and config_remote:
+        raise ValueError('"config_checker" and "config_remote" are mutually exclusive')
 
 
 def get_environment_name() -> str:
@@ -265,6 +266,10 @@ def get_flake8_dependencies() -> List[str]:
         "flake8-unused-arguments",
         "pep8-naming",
     ]
+
+
+def get_github_file(filename: str) -> str:
+    return f"https://raw.githubusercontent.com/dycw/pre-commit-hooks/master/{filename}"
 
 
 def get_pre_commit_repos() -> Dict[str, Dict[str, Any]]:
