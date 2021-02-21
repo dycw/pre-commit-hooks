46a5a5294de375ac3f01d33a12263fd26418c2e4
Derek Wan <d.wan@icloud.com>
2021-02-21 13:55:53 +0800

    Add check of mypy and pytest

--- hooks/check_settings.py
+++ hooks/check_settings.py
@@ -1,5 +1,6 @@
 import sys
 from argparse import ArgumentParser
+from configparser import ConfigParser
 from functools import lru_cache
 from logging import basicConfig
 from logging import INFO
@@ -111,6 +112,25 @@ def check_local_vs_remote(filename: str, url: str) -> None:
             write_local(local_path, url)
 
 
+def check_mypy_ini(file: TextIO) -> None:
+    actual = file.read()
+    expected = """[mypy]
+allow_redefinition = True
+disallow_incomplete_defs = True
+disallow_untyped_decorators = True
+follow_imports = silent
+ignore_missing_imports = True
+platform = linux
+python_version = 3.9
+warn_no_return = True
+warn_redundant_casts = True
+warn_unused_configs = True
+warn_unused_ignores = True
+"""
+    if actual != expected:
+        raise ValueError(f"Actual=\n{actual}\n\nexpected=\n{expected}")
+
+
 def check_pre_commit_config() -> None:
     check_repo(
         repo_url="https://github.com/asottile/add-trailing-comma",
@@ -145,6 +165,11 @@ def check_pre_commit_config() -> None:
         hook_additional_dependencies={"flake8": get_flake8_dependencies()},
         config_filename=".flake8",
         config_remote="https://raw.githubusercontent.com/dycw/pre-commit-hooks/master/.flake8",
+    )
+    check_repo(
+        repo_url="https://github.com/pre-commit/mirrors-mypy",
+        config_filename="mypy.ini",
+        config_checker=check_mypy_ini,
     )
     check_repo(
         repo_url="https://github.com/pre-commit/pre-commit-hooks",
@@ -190,6 +215,25 @@ disable=
 """
     if actual != expected:
         raise ValueError(f"Actual=\n{actual}\n\nexpected=\n{expected}")
+
+
+def check_pytest_ini() -> None:
+    parser = ConfigParser()
+    with open(get_repo_root().joinpath("pytest.ini")) as file:
+        parser.read_file(file)
+    pytest = parser["pytest"]
+    addopts = pytest["addopts"].strip("\n").splitlines()
+    if addopts != sorted(addopts):
+        raise ValueError(f"addopts is unsorted: {addopts}")
+    for opt in ["-q", "-rsxX", "--color=yes"]:
+        if opt not in addopts:
+            raise ValueError(f"addopts missing: {opt}")
+    looponfailroots = pytest["looponfailroots"].strip("\n").splitlines()
+    for path in ["tests"]:
+        if path not in looponfailroots:
+            raise ValueError(f"looponfailroots missing: {path}")
+    if (minversion := pytest["minversion"]) != "6.0":
+        raise ValueError(f"Incorrect min version: {minversion}")
 
 
 def get_flake8_dependencies() -> List[str]:
@@ -250,6 +294,8 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     for filename in args.filenames:
         if filename == ".pre-commit-config.yaml":
             check_pre_commit_config()
+    if get_repo_root().joinpath("tests").exists():
+        check_pytest_ini()
     info("ok...")
     return 0
 
--- setup.py
+++ setup.py
@@ -1,65 +1,17 @@
-from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name="dummy-package",
     entry_points={
         "console_scripts": [
-            "do-python = hooks.do_python:main",
-            "no-commit = hooks.no_commit:main",
-            "notebook = hooks.notebook:main",
-            "text = hooks.text:main",
-            "universal = hooks.universal:main",
+            "check-settings = hooks.check_settings:main",
         ],
     },
     include_package_data=True,
     install_requires=[
         "gitpython",
         "pyyaml",
-        # generic
-        "pre-commit-hooks",
-        # notebooks
-        "nbqa",
-        "nbstripout",
-        # python - formatters
-        "add-trailing-comma",
-        "autoflake",
-        "black",
-        "pyupgrade",
-        "reorder-python-imports",
-        "yesqa",
-        # python - linters
-        "flake8",
-        "mypy",
-        # python - linters - flake8
-        "dlint",
-        "flake8-absolute-import",
-        "flake8-annotations",
-        "flake8-bandit",
-        "flake8-bugbear",
-        "flake8-builtins",
-        "flake8-commas",
-        "flake8-comprehensions",
-        "flake8-debugger",
-        "flake8-eradicate",
-        "flake8-executable",
-        "flake8-fine-pytest",
-        "flake8-fixme",
-        "flake8-future-import",
-        "flake8-implicit-str-concat",
-        "flake8-mutable",
-        "flake8-print",
-        "flake8-pytest-style",
-        "flake8-simplify",
-        "flake8-string-format",
-        "flake8-todo",
-        "flake8-typing-imports",
-        "flake8-unused-arguments",
-        "pep8-naming",
-        # text
-        "fix-smartquotes",
+        "toml",
     ],
-    packages=find_packages(),
-    package_data={"python": [".flake8", "mypy.ini"]},
     url="https://github.com/dycw/pre-commit-hooks",
 )
