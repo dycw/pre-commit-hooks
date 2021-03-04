e912f2ccf8877b15d31819c0a7dcc90c9973af4e
Derek Wan <d.wan@icloud.com>
2021-03-04 23:26:31 +0800

    Better implementation checks for 'subsets' only, warns on extras (#91)
    * Reorganize
    
    * Correct implementation
    
    * Relax the dev deps
    
    * More
    
    * Fix
    
    * Use more generic types

--- src/pre_commit_hooks/check_settings.py
+++ src/pre_commit_hooks/check_settings.py
@@ -4,18 +4,20 @@ from argparse import ArgumentParser
 from collections.abc import Callable
 from collections.abc import Mapping
 from collections.abc import Sequence
-from configparser import ConfigParser
 from functools import lru_cache
 from logging import INFO
 from logging import basicConfig
 from logging import info
+from logging import warning
 from pathlib import Path
 from typing import Any
+from typing import Iterable
 from typing import Optional
 from urllib.request import urlopen
 
 import toml
 import yaml
+from frozendict import frozendict
 from git import Repo
 
 
@@ -23,21 +25,34 @@ basicConfig(level=INFO)
 
 
 def check_black() -> None:
-    actual = read_pyproject_toml_tool()["black"]
+    config = read_pyproject_toml_tool()["black"]
     expected = {
         "line-length": 80,
         "skip-magic-trailing-comma": True,
         "target-version": ["py38"],
     }
-    check_dicts_equal(actual, expected, "black")
+    check_value_or_values(config, expected)
 
 
-def check_dicts_equal(
-    actual: dict[str, Any], expected: dict[str, Any], desc: str
-) -> None:
-    check_lists_equal(list(actual), list(expected), desc)
-    for key in actual:
-        check_key_equals(actual, key, expected[key])
+def check_value_or_values(actual: Any, expected: Any) -> None:
+    if is_iterable(actual) and is_iterable(expected):
+        if isinstance(actual, Mapping) and isinstance(expected, Mapping):
+            for key, exp_val in expected.items():
+                try:
+                    check_value_or_values(actual[key], exp_val)
+                except KeyError:
+                    raise ValueError(f"Missing key: {key}")
+            desc = "key"
+        else:
+            for exp_val in expected:
+                if freeze(exp_val) not in freeze(actual):
+                    raise ValueError(f"Missing value: {exp_val}")
+            desc = "value"
+        for extra in set(freeze(actual)) - set(freeze(expected)):
+            warning(f"Extra {desc} found: {extra}")
+    else:
+        if actual != expected:
+            raise ValueError(f"Differing values found: {actual} != {expected}")
 
 
 def check_flake8() -> None:
@@ -56,17 +71,17 @@ def check_flake8() -> None:
 
 
 def check_hook_fields(
-    repo_hooks: dict[str, Any],
-    expected_mapping: dict[str, list[str]],
+    repo_hooks: Mapping[str, Any],
+    expected: Mapping[str, Iterable[str]],
     field: str,
 ) -> None:
-    for hook_name, expected in expected_mapping.items():
-        current = repo_hooks[hook_name][field]
-        check_lists_equal(current, expected, f"{hook_name}.{field}")
+    for hook, value in expected.items():
+        current = repo_hooks[hook][field]
+        check_value_or_values(current, value)
 
 
 def check_isort() -> None:
-    actual = read_pyproject_toml_tool()["isort"]
+    config = read_pyproject_toml_tool()["isort"]
     expected = {
         "atomic": True,
         "float_to_top": True,
@@ -79,25 +94,7 @@ def check_isort() -> None:
         "src_paths": ["src"],
         "virtual_env": ".venv/bin/python",
     }
-    check_dicts_equal(actual, expected, "isort")
-
-
-def check_key_equals(config: dict[str, Any], key: str, expected: Any) -> None:
-    if config[key] != expected:
-        raise ValueError(f"Incorrect {key!r}; expected {expected}")
-
-
-def check_lists_equal(
-    current: list[str], expected: list[str], desc: str
-) -> None:
-    if current != sorted(current):
-        raise ValueError(f"{desc} actual is unsorted: {current}")
-    if expected != sorted(expected):
-        raise ValueError(f"{desc} expected is unsorted: {expected}")
-    if extra := set(current) - set(expected):
-        raise ValueError(f"{desc} has extra: {extra}")
-    if missing := set(expected) - set(current):
-        raise ValueError(f"{desc} is missing: {missing}")
+    check_value_or_values(config, expected)
 
 
 def check_pre_commit_config_yaml() -> None:
@@ -175,40 +172,48 @@ def check_pre_commit_config_yaml() -> None:
 def check_pyrightconfig() -> None:
     with open(get_repo_root().joinpath("pyrightconfig.json")) as file:
         config = json.load(file)
-    check_key_equals(config, "include", ["src"])
-    check_key_equals(config, "venvPath", ".venv")
-    check_key_equals(config, "executionEnvironments", [{"root": "src"}])
+    expected = {
+        "include": ["src"],
+        "venvPath": ".venv",
+        "executionEnvironments": [{"root": "src"}],
+    }
+    check_value_or_values(config, expected)
 
 
-def check_pytest_ini(path: Path) -> None:
-    # this is not current activated
-    parser = ConfigParser()
-    with open(get_repo_root().joinpath(path)) as file:
-        parser.read_file(file)
-    pytest = parser["pytest"]
-    addopts = pytest["addopts"].strip("\n").splitlines()
-    if addopts != sorted(addopts):
-        raise ValueError(f"addopts is unsorted: {addopts}")
-    for opt in ["-q", "-rsxX", "--color=yes"]:
-        if opt not in addopts:
-            raise ValueError(f"addopts missing: {opt}")
-    looponfailroots = pytest["looponfailroots"].strip("\n").splitlines()
-    for root in ["tests"]:
-        if root not in looponfailroots:
-            raise ValueError(f"looponfailroots missing: {root}")
-    if (minversion := pytest["minversion"]) != "6.0":
-        raise ValueError(f"Incorrect min version: {minversion}")
+def check_pytest() -> None:
+    config = read_pyproject_toml_tool()["pytest"]["ini_options"]
+    expected = {
+        "addopts": [
+            "-q",
+            "-rsxX",
+            "--color=yes",
+            "--instafail",
+            "--strict-markers",
+        ],
+        "minversion": 6.0,
+        "looponfailroots": ["src"],
+        "testpaths": ["src/tests"],
+        "xfailstrict": True,
+        "log_level": "WARNING",
+        "log_cli_datefmt": "%Y-%m-%d %H:%M:%S",
+        "log_cli_format": (
+            "[%(asctime)s.%(msecs)03d] [%(levelno)d] [%(name)s:%(funcName)s] "
+            "[%(process)d]\n%(msg)s"
+        ),
+        "log_cli_level": "WARNING",
+    }
+    check_value_or_values(config, expected)
 
 
 def check_repo(
-    repos: dict[str, dict[str, Any]],
+    repos: Mapping[str, Mapping[str, Any]],
     repo_url: str,
     *,
-    enabled_hooks: Optional[list[str]] = None,
-    hook_args: Optional[dict[str, list[str]]] = None,
-    hook_additional_dependencies: Optional[dict[str, list[str]]] = None,
+    enabled_hooks: Optional[Iterable[str]] = None,
+    hook_args: Optional[Mapping[str, Iterable[str]]] = None,
+    hook_additional_dependencies: Optional[Mapping[str, Iterable[str]]] = None,
     config_checker: Optional[Callable] = None,
-    # Callabe is bugged - https://bit.ly/3bapBly
+    # Callable is bugged - https://bit.ly/3bapBly
 ) -> None:
     try:
         repo = repos[repo_url]
@@ -217,21 +222,27 @@ def check_repo(
 
     repo_hooks = get_repo_hooks(repo)
     if enabled_hooks is not None:
-        check_lists_equal(
-            current=list(repo_hooks), expected=enabled_hooks, desc="hook set"
-        )
+        check_value_or_values(repo_hooks, enabled_hooks)
     if hook_args is not None:
         check_hook_fields(repo_hooks, hook_args, "args")
     if hook_additional_dependencies is not None:
         check_hook_fields(
             repo_hooks, hook_additional_dependencies, "additional_dependencies"
         )
-
     if config_checker is not None:
         config_checker()
 
 
-def get_flake8_extensions() -> list[str]:
+def freeze(x: Any) -> Any:
+    if isinstance(x, Mapping):
+        return frozendict({k: freeze(v) for k, v in x.items()})
+    elif is_iterable(x):
+        return frozenset(map(freeze, x))
+    else:
+        return x
+
+
+def get_flake8_extensions() -> Iterable[str]:
     return read_remote(get_github_file("flake8-extensions")).splitlines()
 
 
@@ -239,7 +250,7 @@ def get_github_file(filename: str) -> str:
     return f"https://raw.githubusercontent.com/dycw/pre-commit-hooks/master/{filename}"
 
 
-def get_pre_commit_repos() -> dict[str, dict[str, Any]]:
+def get_pre_commit_repos() -> Mapping[str, Mapping[str, Any]]:
     with open(get_repo_root().joinpath(".pre-commit-config.yaml")) as file:
         config = yaml.safe_load(file)
     repo = "repo"
@@ -249,7 +260,7 @@ def get_pre_commit_repos() -> dict[str, dict[str, Any]]:
     }
 
 
-def get_repo_hooks(repo: dict[str, Any]) -> dict[str, Any]:
+def get_repo_hooks(repo: Mapping[str, Any]) -> Mapping[str, Any]:
     id_ = "id"
     return {
         mapping[id_]: {k: v for k, v in mapping.items() if k != id_}
@@ -259,9 +270,13 @@ def get_repo_hooks(repo: dict[str, Any]) -> dict[str, Any]:
 
 def get_repo_root() -> Path:
     path = Repo(".", search_parent_directories=True).working_tree_dir
-    if not isinstance(path, str):
-        raise ValueError(f"Invalid path: {path}")
-    return Path(path)
+    if isinstance(path, str):
+        return Path(path)
+    raise ValueError(f"Invalid path: {path}")
+
+
+def is_iterable(x: Any) -> bool:
+    return isinstance(x, Iterable) and not isinstance(x, str)
 
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
@@ -276,6 +291,13 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             check_flake8()
         elif name == ".pre-commit-config.yaml":
             check_pre_commit_config_yaml()
+        elif name == "pyproject.toml":
+            config = read_pyproject_toml_tool()["poetry"]
+            if (
+                "pytest" in config["dependencies"]
+                or "pytest" in config["dev-dependencies"]
+            ):
+                check_pytest()
         elif name == "pyrightconfig.json":
             check_pyrightconfig()
     return 0
