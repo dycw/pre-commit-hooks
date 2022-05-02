bfc967802afc67e1168ff263d3531a5aa24667ee
Derek Wan <d.wan@icloud.com>
2022-05-02 09:20:41 +0800

    Fix

--- src/pre_commit_hooks/run_scan_deps.py
+++ src/pre_commit_hooks/run_scan_deps.py
@@ -3,10 +3,10 @@ from logging import basicConfig
 from logging import error
 from logging import info
 from re import search
-from subprocess import CalledProcessError  # noqa: S404
 from subprocess import check_output  # noqa: S404
 from sys import stdout
 from typing import List
+from typing import Optional
 
 
 basicConfig(level="INFO", stream=stdout)
@@ -18,28 +18,26 @@ def main() -> int:
 
 def _process() -> bool:
     deps = _scan_deps()
-    if len(deps) >= 1:
-        info(deps)
+    if deps is None:
         return False
     else:
-        return True
+        if len(deps) >= 1:
+            info(deps)
+            return False
+        else:
+            return True
 
 
-def _scan_deps() -> List[str]:
+def _scan_deps() -> Optional[List[str]]:
     cmd = ["scan-deps", "poetry.lock", "pyproject.toml"]
     try:
         lines = check_output(cmd, text=True).rstrip("\n")  # noqa: S603
-        error("hi")
-    except CalledProcessError as cperror:
-        error("hi2")
-        if cperror.returncode != 1:
-            error("Failed to run %r", " ".join(cmd))
-        raise
     except FileNotFoundError:
-        error("Failed to run %r. Is `bump2version` installed?", " ".join(cmd))
-        raise
-    except Exception:
-        raise
+        error(
+            "Failed to run %r. Is `poetry-deps-scanner` installed?",
+            " ".join(cmd),
+        )
+        return None
     else:
         return [line for line in lines if search(r"^direct\s+", lines)]
 
