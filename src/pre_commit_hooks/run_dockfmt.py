9630963cc3c5bcb22f8d312baf449888afd9d21d
Derek Wan <d.wan@icloud.com>
2023-01-04 23:41:49 +0800

    Enable all `ruff` lints

--- src/pre_commit_hooks/run_dockfmt.py
+++ src/pre_commit_hooks/run_dockfmt.py
@@ -2,15 +2,15 @@
 from argparse import ArgumentParser
 from logging import basicConfig
 from pathlib import Path
-from subprocess import check_output  # noqa: S404
+from subprocess import check_output
 from sys import argv
 from sys import stdout
-
 
 basicConfig(level="INFO", stream=stdout)
 
 
 def main() -> int:
+    """CLI for the `run_dockfmt` hook."""
     parser = ArgumentParser()
     _ = parser.add_argument("filenames", nargs="*", help="Filenames to check.")
     args = parser.parse_args(argv)
@@ -26,15 +26,14 @@ def _is_dockerfile(filename: str) -> bool:
 def _process(filename: str) -> bool:
     with open(filename) as fh:
         current = fh.read()
-    proposed = check_output(  # noqa: S603, S607
-        ["dockfmt", "fmt", filename], text=True
-    ).lstrip("\t\n")
+    proposed = check_output(["dockfmt", "fmt", filename], text=True).lstrip(
+        "\t\n"
+    )
     if current == proposed:
         return True
-    else:
-        with open(filename, mode="w") as fh:
-            _ = fh.write(proposed)
-        return False
+    with open(filename, mode="w") as fh:
+        _ = fh.write(proposed)
+    return False
 
 
 if __name__ == "__main__":
