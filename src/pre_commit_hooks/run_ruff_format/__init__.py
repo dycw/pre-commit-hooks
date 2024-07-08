ae2ef7af03489a2d01d7beeb0025c7a0aed1f9ab
Derek Wan <d.wan@icloud.com>
2024-07-08 19:13:17 +0800

    Fix `run_ruff_format` to fail gracefully (#261)

--- src/pre_commit_hooks/run_ruff_format/__init__.py
+++ src/pre_commit_hooks/run_ruff_format/__init__.py
@@ -1,18 +1,14 @@
 from __future__ import annotations
 
-from contextlib import contextmanager
 from subprocess import CalledProcessError, check_call
-from typing import TYPE_CHECKING, cast
+from typing import cast
 
 from click import command
 from loguru import logger
-from tomlkit import TOMLDocument, dumps, table
+from tomlkit import dumps, table
 from tomlkit.container import Container
 
-from pre_commit_hooks.common import PYPROJECT_TOML, read_pyproject
-
-if TYPE_CHECKING:
-    from collections.abc import Iterator
+from pre_commit_hooks.common import PYPROJECT_TOML, PyProject, read_pyproject
 
 
 @command()
@@ -22,24 +18,15 @@ def main() -> bool:
 
 
 def _process() -> bool:
-    with _yield_modified_pyproject():
-        result1 = _run_ruff_format()
-    result2 = _run_ruff_format()
+    curr = read_pyproject()
+    new = _get_modified_pyproject()
+    result1 = _run_ruff_format(new)
+    result2 = _run_ruff_format(curr)
+    _write_pyproject(curr)
     return result1 and result2
 
 
-@contextmanager
-def _yield_modified_pyproject() -> Iterator[None]:
-    curr = read_pyproject()
-    new = _get_modified_pyproject()
-    with PYPROJECT_TOML.open(mode="w") as fh:
-        _ = fh.write(dumps(new))
-    yield
-    with PYPROJECT_TOML.open(mode="w") as fh:
-        _ = fh.write(curr.contents)
-
-
-def _get_modified_pyproject() -> TOMLDocument:
+def _get_modified_pyproject() -> PyProject:
     pyproject = read_pyproject()
     doc = pyproject.doc
     try:
@@ -70,14 +57,20 @@ def _get_modified_pyproject() -> TOMLDocument:
     ruff["format"] = format_
     ruff["lint"] = lint
     lint["isort"] = isort
-    return doc
+    return PyProject(contents=dumps(doc), doc=doc)
 
 
-def _run_ruff_format() -> bool:
+def _run_ruff_format(pyproject: PyProject, /) -> bool:
+    _write_pyproject(pyproject)
     cmd = ["ruff", "format", "."]
     try:
         code = check_call(cmd)
     except CalledProcessError:
         logger.exception("Failed to run {cmd!r}", cmd=" ".join(cmd))
-        raise
+        return False
     return code == 0
+
+
+def _write_pyproject(pyproject: PyProject, /) -> None:
+    with PYPROJECT_TOML.open(mode="w") as fh:
+        _ = fh.write(pyproject.contents)
