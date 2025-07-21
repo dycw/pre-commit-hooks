491dc45037fa3a57dce962803fa3f335ce814861
Derek Wan <d.wan@icloud.com>
2025-07-21 11:15:05 +0900

    save

--- src/tests/format_requirements/test_format_requirements.py
+++ src/tests/format_requirements/test_format_requirements.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from pathlib import Path
 
 from pytest import fixture
+from tomlkit import dumps
 
 from pre_commit_hooks.format_requirements import _format
 
@@ -15,6 +16,6 @@ def toml() -> Path:
 class TestFormatRequirements:
     def test_basic(self, *, toml: Path) -> None:
         root = toml.joinpath("basic")
-        result = _format(root.joinpath("in.toml"))
-        expected = root.joinpath("out.toml")
+        result = dumps(_format(root.joinpath("in.toml")))
+        expected = root.joinpath("out.toml").read_text()
         assert result == expected
