1296172015f8ebf507d351de0129c757523a4fef
Derek Wan <d.wan@icloud.com>
2025-08-31 09:13:07 +0900

    2025-08-31 09:13:07 (Sun) > DW-Mac > derekwan

--- src/pre_commit_hooks/common.py
+++ src/pre_commit_hooks/common.py
@@ -12,55 +12,45 @@ from utilities.version import Version, parse_version
 PYPROJECT_TOML = get_repo_root().joinpath("pyproject.toml")
 
 
-def get_version(
-    path_or_text: Path | str | bytes | TOMLDocument, /, *, desc: str = ""
-) -> Version:
-    """Parse the version from a block of text."""
-    match path_or_text:
+def get_bumpversion_version(source: Path | str | bytes | TOMLDocument, /) -> Version:
+    """Get the `[tool.bumpversion]` version from a TOML file."""
+    match source:
         case Path() as path:
-            return get_version(
-                path.read_text(), desc=f" from {str(path)!r}" if desc == "" else desc
-            )
+            return get_bumpversion_version(path.read_text())
         case str() | bytes() as text:
-            return get_version(parse(text), desc=desc)
+            return get_bumpversion_version(parse(text))
         case TOMLDocument() as doc:
             try:
                 tool = doc["tool"]
             except KeyError:
-                logger.exception(
-                    f"Failed to get version{desc}; key 'tool' does not exist"
-                )
+                logger.exception("Failed to get version; key 'tool' does not exist")
                 raise
             if not isinstance(tool, Table):
-                logger.exception(f"Failed to get version{desc}; `tool` is not a Table")
+                logger.exception("Failed to get version; `tool` is not a Table")
                 raise TypeError
             try:
                 bumpversion = tool["bumpversion"]
             except KeyError:
                 logger.exception(
-                    f"Failed to get version{desc}; key 'bumpversion' does not exist"
+                    "Failed to get version; key 'bumpversion' does not exist"
                 )
                 raise
             if not isinstance(bumpversion, Table):
-                logger.exception(
-                    f"Failed to get version{desc}; `bumpversion` is not a Table"
-                )
+                logger.exception("Failed to get version; `bumpversion` is not a Table")
                 raise TypeError
             try:
                 version = bumpversion["current_version"]
             except KeyError:
                 logger.exception(
-                    f"Failed to get version{desc}; key 'current_version' does not exist"
+                    "Failed to get version; key 'current_version' does not exist"
                 )
                 raise
             if not isinstance(version, str):
-                logger.exception(
-                    f"Failed to get version{desc}; `version` is not a string"
-                )
+                logger.exception("Failed to get version; `version` is not a string")
                 raise TypeError
             return parse_version(version)
         case never:  # pyright: ignore[reportUnnecessaryComparison]
             assert_never(never)
 
 
-__all__ = ["PYPROJECT_TOML", "get_version"]
+__all__ = ["PYPROJECT_TOML", "get_bumpversion_version"]
--- src/pre_commit_hooks/run_bump_my_version/__init__.py
+++ src/pre_commit_hooks/run_bump_my_version/__init__.py
@@ -2,25 +2,36 @@ from __future__ import annotations
 
 from pathlib import Path
 from subprocess import PIPE, STDOUT, CalledProcessError, check_call, check_output
+from typing import Literal, assert_never
 
-from click import command
+from click import Choice, command, option
 from loguru import logger
+from utilities.pathlib import get_repo_root
 
-from pre_commit_hooks.common import PYPROJECT_TOML, get_version
+from pre_commit_hooks.common import PYPROJECT_TOML, get_bumpversion_version
+
+type _Mode = Literal["pyproject", "bumpversion"]
+_MODE: _Mode = "pyproject"
 
 
 @command()
-def main() -> bool:
+@option(
+    "--mode",
+    type=Choice(["pyproject", "bumpversion"], case_sensitive=False),
+    default=_MODE,
+    show_default=True,
+)
+def main(*, mode: _Mode = _MODE) -> bool:
     """CLI for the `run_bump_my_version` hook."""
-    return _process()
+    return _process(mode=mode)
 
 
-def _process() -> bool:
-    path = PYPROJECT_TOML.relative_to(Path.cwd())
-    current = get_version(path)
+def _process(*, mode: _Mode = _MODE) -> bool:
+    path = _get_rel_path(mode=mode)
+    current = get_bumpversion_version(path)
     commit = check_output(["git", "rev-parse", "origin/master"], text=True).rstrip("\n")
     contents = check_output(["git", "show", f"{commit}:{path}"], text=True)
-    master = get_version(contents)
+    master = get_bumpversion_version(contents)
     if current in {master.bump_patch(), master.bump_minor(), master.bump_major()}:
         return True
     cmd = [
@@ -42,6 +53,17 @@ def _process() -> bool:
     else:
         return True
     return False
+
+
+def _get_rel_path(*, mode: _Mode = _MODE) -> Path:
+    match mode:
+        case "pyproject":
+            path = PYPROJECT_TOML
+        case "bumpversion":
+            path = get_repo_root().joinpath(".bumpversion.toml")
+        case never:  # pyright: ignore[reportUnnecessaryComparison]
+            assert_never(never)
+    return path.relative_to(Path.cwd())
 
 
 __all__ = ["main"]
