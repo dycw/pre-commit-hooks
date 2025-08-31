dbdd38be359888fa57c321e59601fdc3394c3f68
Derek Wan <d.wan@icloud.com>
2025-08-31 21:34:47 +0900

    Add `run_every` option to `mirror-files` (#308)

--- src/pre_commit_hooks/mirror_files/__init__.py
+++ src/pre_commit_hooks/mirror_files/__init__.py
@@ -8,26 +8,37 @@ from loguru import logger
 from more_itertools import chunked
 from utilities.atomicwrites import writer
 
+from pre_commit_hooks.common import run_every_option, throttled_run
+
 if TYPE_CHECKING:
+    from collections.abc import Iterable
     from pathlib import Path
+
+    from whenever import DateTimeDelta
 
 
 @command()
 @argument("paths", nargs=-1, type=utilities.click.Path())
-def main(*, paths: tuple[Path, ...]) -> bool:
+@run_every_option
+def main(*, paths: tuple[Path, ...], run_every: DateTimeDelta | None = None) -> bool:
     """CLI for the `format-requirements` hook."""
+    return throttled_run("mirror-files", run_every, _process, paths)
+
+
+def _process(paths: Iterable[Path], /) -> bool:
+    paths = list(paths)
     if len(paths) % 2 == 1:
         logger.exception(f"Expected an even number of paths; got {len(paths)}")
         raise RuntimeError
-    results = list(map(_process, chunked(paths, 2, strict=True)))  # run all
+    results = list(map(_process_pair, chunked(paths, 2, strict=True)))  # run all
     return all(results)
 
 
-def _process(paths: list[Path], /) -> bool:
-    text_from, text_to = [p.read_text() for p in paths]
+def _process_pair(paths: Iterable[Path], /) -> bool:
+    path_from, path_to = paths
+    text_from, text_to = path_from.read_text(), path_to.read_text()
     if text_from == text_to:
         return True
-    _, path_to = paths
     with writer(path_to, overwrite=True) as temp:
         _ = temp.write_text(text_from)
     return False
