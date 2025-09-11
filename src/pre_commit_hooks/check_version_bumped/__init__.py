bb990e2c6643b08cd888b8a48801871ccd4b0b09
Derek Wan <d.wan@icloud.com>
2025-09-11 12:31:35 +0900

    2025-09-11 12:31:34 (Thu) > DW-Mac > derekwan

--- src/pre_commit_hooks/check_version_bumped/__init__.py
+++ src/pre_commit_hooks/check_version_bumped/__init__.py
@@ -11,19 +11,16 @@ from pre_commit_hooks.common import run_all, run_every_option, throttled_run, wr
 if TYPE_CHECKING:
     from pathlib import Path
 
-    from whenever import DateTimeDelta
-
 
 @command()
-@run_every_option
-def main(*, run_every: DateTimeDelta | None = None) -> bool:
-    """CLI for the `check-submodules-updated` hook."""
-    return throttled_run("check-submodules-updated", run_every, _process)
+def main() -> bool:
+    """CLI for the `check-version-bumped` hook."""
+    return _process()
 
 
 def _process() -> bool:
     file = cast("Path", files("pre_commit_hooks")).joinpath(
-        "check_submodules_updated", "check-submodules-updated"
+        "check_version_bumped", "check-version-bumped"
     )
     try:
         _ = run([str(file)], check=True)
@@ -32,4 +29,4 @@ def _process() -> bool:
     return True
 
 
-__all__ = ["main", "run_all", "write_text"]
+__all__ = ["main", "run_all", "run_every_option", "throttled_run", "write_text"]
