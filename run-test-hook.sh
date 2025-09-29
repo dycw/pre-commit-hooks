f5b32c42eac6ff36b2acdd31f64e2d9d40e1141e
Derek Wan <d.wan@icloud.com>
2025-09-29 22:36:41 +0900

    2025-09-29 22:36:39 (Mon) > DW-Mac > derekwan

--- run-test-hook.sh
+++ run-test-hook.sh
@@ -9,6 +9,6 @@ PATH_DIR="$(
 	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit
 	pwd -P
 )"
-HOOK_NAME='test-commits'
+HOOK_NAME='tag-commits'
 
 pre-commit try-repo --verbose --all-files "${PATH_DIR}" "${HOOK_NAME}"
