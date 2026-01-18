970066818b67f1653eea2bde4fa0d25bae9e7ba1
Derek Wan <d.wan@icloud.com>
2026-01-19 06:39:53 +0900

    Add `add-future-import-annotations` (#349)
    Co-authored-by: github-actions-bot <noreply@github.com>

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from pytest import mark, param
+from utilities.constants import MINUTE
+from utilities.pytest import throttle_test
 from utilities.subprocess import run
 
 from pre_commit_hooks.constants import BUMPVERSION_TOML, PRE_COMMIT_CONFIG_YAML
@@ -10,6 +12,7 @@ class TestCLI:
     @mark.parametrize(
         ("hook", "args"),
         [
+            param("add-future-import-annotations", ["path.py"]),
             param("add-hooks", [str(PRE_COMMIT_CONFIG_YAML)]),
             param("add-ruff-hooks", [str(PRE_COMMIT_CONFIG_YAML)]),
             param("check-versions-consistent", [str(BUMPVERSION_TOML)]),
@@ -17,5 +20,6 @@ class TestCLI:
             param("run-version-bump", [str(BUMPVERSION_TOML)]),
         ],
     )
+    @throttle_test(duration=MINUTE)
     def test_main(self, *, hook: str, args: list[str]) -> None:
         run(hook, *args)
